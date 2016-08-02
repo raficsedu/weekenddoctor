c5a0e61c39a19241c8aac6ef17ea20c52a71fabf
masum <masumwen07@gmail.com>
2016-08-03 02:39:55 +0600

    some bug but i will solve next day

---
+++ app/Specialtiy.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Specialtiy extends Model
+{   
+	protected $table="specialties";
+    public function user(){
+        return $this->belongsTo("App\User"); 
+    }
+    
+}
