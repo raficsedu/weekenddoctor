399ff367e068d234aee08280afd4b4948d826b64
masum <masumwen07@gmail.com>
2016-08-03 21:53:04 +0600

    sign-up version-1 complete

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -8,7 +8,8 @@ use App\Http\Requests;
 
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\DB;
-
+use App\Specialtiy;
+use App\DoctorMeta;
 use App\User;
 use Mail;
 use Auth;
@@ -29,7 +30,9 @@ class RegistrationController extends Controller
 
     public function get_started()
     {
-        return view('pages.get_started');
+        $specialty = Specialtiy::Select('name')->get();
+       //dd($specialty);
+        return view('pages.get_started',['specialties' =>$specialty ]);
     }
 
     public function patient_registration(Request $request)
@@ -60,7 +63,7 @@ class RegistrationController extends Controller
                 'active' => 0,
                 'user_level' => $user_level,
                 'created_at' => date('Y-m-d H:i:s'),]
-            );
+                );
 
             //Sending Confirmation Email
             $data['email'] = $email;
@@ -80,8 +83,71 @@ class RegistrationController extends Controller
         }
         return view('pages.join_us');
     }
+    public function doctor_registration(Request $request)
+    {
+        if ($request->isMethod('post')) {
 
-    public function doctor_registration(Request $request){
-        
-    }
+            $existing_user = User::where('email', $request->email)->count();
+            if($existing_user > 0)
+            {
+                Session::put('message', 'Account already exists');
+                return Redirect('/join-us');
+            }
+
+            $confirmation_code = str_random(10);
+            $user_level = intval($request->input('registration_type'));
+            $email = $request->input('email');
+            $first_name = $request->input('first_name');
+            $last_name = $request->input('last_name');
+
+            $fontEndKey =[  
+                            "Specialty",
+                            "Phone",
+                            "Zip"
+                        ];
+            $fontEndValue =[
+                            $request->input('specialty'),
+                            $request->input('phone'),
+                            $request->input('zip')
+                            ];
+
+            $user_id = DB::table('users')->insertGetId(
+                ['first_name' => $first_name,
+                'last_name' => $last_name,
+                'email' => $email,
+                
+                'remember_token' => $request->input('_token'),
+                'verification_code' => $confirmation_code,
+                'active' => 0,
+                'user_level' => $user_level,
+                'created_at' => date('Y-m-d H:i:s'),]
+                );
+           
+            for ($i=0; $i < sizeof($fontEndKey); $i++) { 
+               DB::table('doctor_metas')->insertGetId(
+                ['user_id' => $user_id,
+                'meta_key' => $fontEndKey[$i],
+                'meta_value' => $fontEndValue[$i],]
+                );
+           }
+          //Sending Confirmation Email
+           $data['email'] = $email;
+           $data['name'] = $first_name.' '.$last_name;
+           $data['user_id'] = $user_id;
+           $data['confirmation_code'] = $confirmation_code;
+
+           Mail::send(['html' => 'email.verify'], $data, function ($m) use ($data) {
+            $m->from('raficsedu@gmail.com', 'Weekend Doctor');
+
+            $m->to($data['email'], $data['name'])->subject('Please verify your email');
+        });
+
+           Session::put('message', 'Thanks for signing up! Please check your email and verify');
+           return redirect()->route('get_started');
+
+       }
+       return view('pages.get_started');
+
+
+   }
 }
--- resources/views/pages/get_started.blade.php
+++ resources/views/pages/get_started.blade.php
@@ -7,134 +7,108 @@
             <h1>Get Started with WeekendDocs</h1>
             <div class="line1"></div>
             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 clearfix" style="padding:0px;">
+                <h4 style="color: red;text-align:left">@if(Session::has('message')){{Session::pull('message','default')}}@endif</h4>
                 <div class="singBody">
                     <form id="signupForm" action="{{url('/doctor-registration')}}" method="post">
-                        {{ csrf_field() }}
-
-                        <input type="hidden" name="registration_type" value="2">
-
-                        <div class="singRow clearfix">
-                            <div class="box-in">
-
-                                <label class="clearfix">First Name</label>
-                                <input type="text" name="first_name" id="first_name" placeholder="First Name" class="txtBox2 month">
-                                <label for="first_name" class="error" id="first_name-error"></label>
-                            </div>
-
-                            <div class="box-in">
-
-                                <label class="clearfix">Last Name</label>
-                                <input type="text" name="last_name" id="last_name" placeholder="Last Name" class="txtBox2 month">
-                                <label for="first_name" class="error" id="last_name-error"></label>
-                            </div>
-
-                            <div class="box-in">
-
-                                <label class="clearfix">Your Speciality</label>
-                                <select name="speciality" id="speciality" class="countryCode txtBox2" type="text" >
-                                    <label for="speciality" class="error" id="speciality-error"></label>
-
-                                </select>
-                            </div>
-
-
-
-
-
-                            <div class="box-in1">
-
-                                <label class="clearfix">Your Direct Phone Number</label>
-                                <input type="text" name="district_phone_number" id="district_phone_number" placeholder="Your Direct Phone Number" class="txtBox2 month">
-                                <label for="district_phone_number" class="error" id="district_phone_number-error"></label>
-                            </div>
-
-                            <div class="box-in1">
-
-                                <label class="clearfix">Zip Code</label>
-                                <input type="text" name="zip_code" id="zip_code" placeholder="Zip Code" class="txtBox2 month">
-                                <label for="zip_code" class="error" id="zip_code-error"></label>
-
-                            </div>
-                            <br>
-                            <br>
-
-
-
-                            <div class="box-in1">
-
-                                <label class="clearfix">Your Email</label>
-                                <input type="text" name="email" id="email" placeholder="Your Email" class="txtBox2 month">
-                                <label for="email" class="error" id="email-error"></label>
-
-                            </div>
-                            <br>
-                            <br>
+                     {{ csrf_field() }}
+                     <input type="hidden" name="registration_type" value="2">
+                     <div class="singRow clearfix">
+                        <div class="box-in">
+                            <label>First Name</label>
+                            <label for="first_name" class="error" class="clearfix"></label>
+                            <input id="first_name" name="first_name" type="text" placeholder="First Name" class="txtBox2 month">
                         </div>
-
-
-
-                        <div class="singRow">
-                            <button type="submit" class="signBtn">Get Started</button>
-                        </div>
-
-                    </form>
+                        <div class="box-in">
+                          <label>Last Name</label>
+                          <label for="last_name" class="error" class="clearfix"></label>
+                          <input id="last_name" name="last_name" type="text" placeholder="Last Name" class="txtBox2 month">
+                      </div>
+                      <div class="box-in">
+                        <label>Your Specialty</label>
+                        <label for="specialty" class="error"  class="clearfix"></label>
+                        <select id="specialty" name="specialty" class="countryCode txtBox2" type="text">
+                          @foreach($specialties as $specialty)
+                          <option value="{{$specialty->name}}">{{$specialty->name}}</option>
+                          @endforeach
+                          
+                      </select>
+                  </div>
+                  <div class="box-in1">
+                    <label>Your DirectPhone Number</label>
+                    <label for="phone" class="error" class="clearfix"></label>
+                    <input id="phone" name="phone" type="text" placeholder="Your Direct Phone Number" class="txtBox2 month">
                 </div>
+                <div class="box-in1">
+                    <label>Zip Code</label>
+                    <label for="zip" class="error" class="clearfix"></label>
+                    <input id="zip" name="zip" type="text" placeholder="Zip Code" class="txtBox2 month">
+                </div>
+                <br>
+                <br>
+                <div class="box-in1">
+                    <label>Your Email</label>
+                    <label for="email" id="confirm_email-error" class="error" class="clearfix"></label>
+                    <input id="email" name="email" type="text" placeholder="Your Email" class="txtBox2 month">
+                </div>
+                <br>
+                <br>
             </div>
+            <div class="singRow">
+                <button type="submit" class="signBtn" style="background:#298DC6;">Get Started</button>
 
-        </section>
-
+            </div>
+        </form>
     </div>
+</div>
+</section>
+</div>
 </section>
 @endsection
-
 @section('footer_custom_script')
-    <script>
-        jQuery(function($) {
-
-            // validate signup form on keyup and submit
-            $("#signupForm").validate({
-                rules: {
-                    first_name: {
-                        required: true
-                    },
-                    last_name: {
-                        required: true
-                    },
-                    speciality: {
-                        required: true
-                    },
-                    district_phone_number: {
-                        required: true
-                    },
-                    zip_code: {
-                        required: true
-                    },
-                    email: {
-                        required: true,
-                        email: true
-                    },
+<script>
+    jQuery(function($) {
+        // validate signup form on keyup and submit
+        $("#signupForm").validate({
+            rules: {
+                first_name: {
+                    required: true
                 },
-                messages: {
-
-                    first_name: {
-                        required: "Please provide your first name"
-                    },
-                    last_name: {
-                        required: "Please provide your last name"
-                    },
-                    speciality: {
-                        required: "Please provide your speciality"
-                    },
-                    district_phone_number: {
-                        required: "Please provide your district phone number"
-                    },
-                    zip_code: {
-                        required: "Please provide your zip_code"
-                    },
-                    email: "Please enter a valid email address",
-
+                last_name: {
+                    required: true
+                },
+                specialty: {
+                    required: true
+                },
+                phone: {
+                    required: true
+                },
+                zip: {
+                    required: true
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+            },
+            messages: {
+                first_name: "Please provide your first name",
+                last_name: {
+                    required: "Please provide your last name",
+                },
+                specialty: {
+                    required: "Please provide your Specialty",
+                },
+                phone: {
+                    required: "Please provide your Phone No."
+                },
+                zip: {
+                    required: "Please provide your Zip Code"
+                },
+                email: {
+                    required: "Please enter a valid email address"
                 }
-            });
+            }
         });
-    </script>
+});
+</script>
 @endsection
