e18916944ee7c60cb254ba698b41a9da1d2a44ef
Md. Muntasir Rahman <raficsedu@gmail.com>
2016-08-03 21:44:46 +0600

    Fixing validation

--- resources/views/pages/get_started.blade.php
+++ resources/views/pages/get_started.blade.php
@@ -10,79 +10,52 @@
                 <div class="singBody">
                     <form id="signupForm" action="{{url('/doctor-registration')}}" method="post">
                         {{ csrf_field() }}
-
                         <input type="hidden" name="registration_type" value="2">
-
                         <div class="singRow clearfix">
                             <div class="box-in">
-
                                 <label class="clearfix">First Name</label>
                                 <input type="text" name="first_name" id="first_name" placeholder="First Name" class="txtBox2 month">
-                                <label for="first_name" class="error" id="first_name-error"></label>
+                                <div style="width:100%;float:left;"><label for="first_name" class="error" id="first_name-error"></label></div>
                             </div>
-
                             <div class="box-in">
-
                                 <label class="clearfix">Last Name</label>
                                 <input type="text" name="last_name" id="last_name" placeholder="Last Name" class="txtBox2 month">
-                                <label for="first_name" class="error" id="last_name-error"></label>
+                                <div style="width:100%;float:left;"><label for="last_name" class="error" id="last_name-error"></label></div>
                             </div>
-
                             <div class="box-in">
-
                                 <label class="clearfix">Your Speciality</label>
                                 <select name="speciality" id="speciality" class="countryCode txtBox2" type="text" >
-                                    <label for="speciality" class="error" id="speciality-error"></label>
 
                                 </select>
+                                <div style="width:100%;float:left;"><label for="speciality" class="error" id="speciality-error"></label></div>
                             </div>
-
-
-
-
-
                             <div class="box-in1">
-
                                 <label class="clearfix">Your Direct Phone Number</label>
                                 <input type="text" name="district_phone_number" id="district_phone_number" placeholder="Your Direct Phone Number" class="txtBox2 month">
-                                <label for="district_phone_number" class="error" id="district_phone_number-error"></label>
+                                <div style="width:100%;float:left;"><label for="district_phone_number" class="error" id="district_phone_number-error"></label></div>
                             </div>
-
                             <div class="box-in1">
-
                                 <label class="clearfix">Zip Code</label>
                                 <input type="text" name="zip_code" id="zip_code" placeholder="Zip Code" class="txtBox2 month">
-                                <label for="zip_code" class="error" id="zip_code-error"></label>
-
+                                <div style="width:100%;float:left;"><label for="zip_code" class="error" id="zip_code-error"></label></div>
                             </div>
                             <br>
                             <br>
-
-
-
                             <div class="box-in1">
-
                                 <label class="clearfix">Your Email</label>
                                 <input type="text" name="email" id="email" placeholder="Your Email" class="txtBox2 month">
-                                <label for="email" class="error" id="email-error"></label>
-
+                                <div style="width:100%;float:left;"><label for="email" class="error" id="email-error"></label></div>
                             </div>
                             <br>
                             <br>
                         </div>
-
-
-
                         <div class="singRow">
                             <button type="submit" class="signBtn">Get Started</button>
                         </div>
-
                     </form>
                 </div>
             </div>
-
         </section>
-
     </div>
 </section>
 @endsection
@@ -112,7 +85,7 @@
                     email: {
                         required: true,
                         email: true
-                    },
+                    }
                 },
                 messages: {
 
